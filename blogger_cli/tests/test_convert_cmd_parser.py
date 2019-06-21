1c032749b43d9a83476504dc8b3e5165b1915d5a
Hemanta Sharma <sharmahemanta.212@gmail.com>
2019-06-21 22:26:39 +0545

    added test for get_files_being_converted func

--- blogger_cli/tests/test_convert_cmd_parser.py
+++ blogger_cli/tests/test_convert_cmd_parser.py
@@ -1,13 +1,16 @@
 import os
 import shutil
 import unittest
-from tempfile import tempdir, tempfile
 
-from blogger_cli.commands.cmd_convert import (get_files_being_converted,
-    get_all_files, check_and_ensure_destination_dir)
+from tempfile import mkdtemp
+from pkg_resources import resource_filename
+
+from click.testing import CliRunner
 from blogger_cli import root_dir
 from blogger_cli.cli import Context
-from pkg_resources import resource_filename
+from blogger_cli.cli import cli
+from blogger_cli.commands.cmd_convert import (get_files_being_converted,
+    check_and_ensure_destination_dir)
 
 
 class TestBasic(unittest.TestCase):
@@ -15,7 +18,7 @@ class TestBasic(unittest.TestCase):
     def setUp(self):
         self.runner = CliRunner()
         self.ctx = Context()
-        self.export_dir = tempdir
+        self.export_dir = mkdtemp()
         self.blog_dir = os.path.join(self.export_dir, 'blog')
         self.index_path = os.path.join(self.blog_dir, 'index.html')
         self.runner.invoke(cli, ['addblog', 'test1'],
@@ -25,9 +28,31 @@ class TestBasic(unittest.TestCase):
         self.runner.invoke(cli, ['config', '-b', 'test1',
                                  'blog_images_dir', 'images'])
 
-    def test_get_files_being_converted(self):
-         resource_path = resource_filename('blogger_cli',
-                    'tests/tests_resources/')
-        files = get_files_being_converted(resource_path)
-        expected_files = {'html.html', 'ipynb1.ipynb', 'ipynb2.ipynb', 'md1.md'
 
+    def test_get_files_being_converted(self):
+        resource_path = os.path.join(root_dir, 'tests', 'tests_resources')
+        files = get_files_being_converted( (resource_path, ) )
+        expected_files = {'html.html', 'ipynb1.ipynb', 'ipynb2.ipynb',
+                        'md1.md', 'md2.md'}
+        expected = {os.path.join(resource_path, i) for i in expected_files}
+        self.assertEqual(expected, files)
+
+        index_files = {'html_index.html', 'ipynb1_index.html',
+                'ipynb2_index.html','md_index.html', 'topic_index.html'}
+        path_join = lambda x: os.path.join(resource_path, 'index', x)
+        expected_files = {path_join(i) for i in index_files}
+        expected.update(expected_files)
+        file1 = os.path.join(resource_path, 'html.html')
+        file2 = os.path.join(resource_path, 'md1.md')
+        files = get_files_being_converted((file1, file2, resource_path),
+                                        recursive=True)
+        self.assertEqual(expected, files)
+
+
+    def tearDown(self):
+        self.runner.invoke(cli, ['rmblog', 'test1'])
+        shutil.rmtree(self.export_dir)
+
+
+if __name__ == '__main__':
+    unittest.main()
